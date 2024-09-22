ba124dbe4654887d08bac19f6b45cf55db5a4913
Schinqy <101190248+Schinqy@users.noreply.github.com>
2024-09-22 03:20:21 +0200

    Add functionality to fetch data and display it on the main activity ui

--- mobile_app/app/src/main/java/com/luitech/abops/MainActivity.java
+++ mobile_app/app/src/main/java/com/luitech/abops/MainActivity.java
@@ -2,17 +2,42 @@ package com.luitech.abops;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageSwitcher;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.luitech.abops.network.ApiClient;
+import com.luitech.abops.network.DataInterface;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.BreakIterator;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
     private CardView flowRateCardView;
     private CardView waterLevelCardView;
+    private TextView waterLevelTextView;
+    private TextView flowRateTextView;
+    private DeviceStatusChecker statusChecker;
+    private TextView deviceStatusTextView;
+    private AppCompatImageView deviceStatusIcon;
 
 
     @Override
@@ -23,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
 
         flowRateCardView = findViewById(R.id.flowRateCard);
         waterLevelCardView = findViewById(R.id.waterLevelCard);
+        waterLevelTextView = findViewById(R.id.waterLevel);
+        flowRateTextView = findViewById(R.id.flowRate);
+        deviceStatusTextView =  findViewById(R.id.deviceStatus);
+        deviceStatusIcon =  findViewById(R.id.deviceStatusIcon);
 
 
         flowRateCardView.setOnClickListener(view -> {
@@ -36,11 +65,105 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("graph_type", "water");
             startActivity(intent);
         });
+        statusChecker = new DeviceStatusChecker(); // Initialize statusChecker
+
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+
+    private void fetchData(String manholeId) {
+        DataInterface dataInterface = ApiClient.getDataInterface();
+        Call<ResponseBody> call = dataInterface.getData(manholeId);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    try {
+                        String responseBody = response.body().string();
+                        // Log.d("DATA RECEIVED", "Data Received" + responseBody);
+                        JSONObject jsonObject = new JSONObject(responseBody);
+                        String status = jsonObject.optString("status");
+
+                        if ("success".equals(status)) {
+                            JSONArray dataArray = jsonObject.optJSONArray("data");
+                            if (dataArray != null && dataArray.length() > 0) {
+                                JSONObject latestData = null;
+                                long latestTimestamp = Long.MIN_VALUE;
+
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+
+                                for (int i = 0; i < dataArray.length(); i++) {
+                                    JSONObject data = dataArray.getJSONObject(i);
+                                    String timestampStr = data.optString("timestamp");
+
+                                    long timestamp;
+                                    try {
+                                        Date date = dateFormat.parse(timestampStr);
+                                        if (date != null) {
+                                            timestamp = date.getTime();
+                                        } else {
+                                            continue;
+                                        }
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                        continue;
+                                    }
+
+                                    if (timestamp > latestTimestamp) {
+                                        latestTimestamp = timestamp;
+                                        latestData = data;
+                                    }
+                                }
+
+                                if (latestData != null) {
+                                    String waterLevel = latestData.optString("water_level");
+                                    String flowRate = latestData.optString("reed_status");
+                                    String timestamp = latestData.optString("timestamp");
+
+
+                                        waterLevelTextView.setText(waterLevel + " L");
+                                        flowRateTextView.setText((flowRate + " L/s"));
+
+                                    boolean isOnline = statusChecker.isDeviceOnline(timestamp);
+
+
+                                    if (isOnline) {
+                                        deviceStatusTextView.setText("Device Online");
+                                        deviceStatusIcon.setImageResource(R.drawable.ic_online);
+                                    } else {
+                                        deviceStatusTextView.setText("Last Seen: " + timestamp);
+                                        deviceStatusIcon.setImageResource(R.drawable.ic_offline);
+
+                                    }
+                                } else {
+                                    deviceStatusTextView.setText("No data found.");
+                                }
+                            } else {
+                                deviceStatusTextView.setText("No data found.");
+                            }
+                        } else {
+                            deviceStatusTextView.setText("Error: " + status);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        deviceStatusTextView.setText("Failed to parse data.");
+                    }
+                } else {
+                    deviceStatusTextView.setText("Request failed.");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                deviceStatusTextView.setText("Failed to fetch data.");
+            }
         });
     }
 }
